f1ee140b808a788a12392dce2e3c1135c2498146
Nick Wilson <nickw0584@icloud.com>
2019-09-11 09:31:54 -0500

    first commit

---
+++ javascript/portfolio.js
@@ -0,0 +1,7 @@
+<img id="Image 2" src="https://media0.giphy.com/media/f4HpCDvF84oh2/200w.webp?cid=790b7611359d80585ccf38ab5a9a1a8eddcda83b553a880f&rid=200w.webp" />
+<img id="Image 2" src="img.webp" 
+onclick="myFunction()" />
+document.getElementById( "Image 2" ).onclick = function() {
+  // img clicked
+};
+<input type="button"/>
